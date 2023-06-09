f7de3766b31ea2b9d4ea9d8e46148868c6de2eac
Marina Pereira <imarinaapereira@gmailcom>
2023-06-09 00:42:54 -0300

    colocando espaço no alert

--- palindromo.js
+++ palindromo.js
@@ -6,7 +6,7 @@ for (let i = palavra.length - 1 ;i >=0 ; i--){
 }
 
 if (palavra === palavraInvertida){
-  alert ( palavra + "é um palindromo")
+  alert ( palavra + " é um palindromo")
 } else{
-  alert (palavra + "não é um palindromo")
+  alert (palavra + " não é um palindromo")
 }
